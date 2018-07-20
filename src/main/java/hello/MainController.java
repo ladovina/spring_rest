ba367cccc258c813e797bc98b86cff4161c91317
Vladimir Svoboda <ladovina@seznam.cz>
2018-07-20 09:37:52 +0200

    Added PostgreSQL DB.

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import hello.User;
-import hello.UserRepository;
-
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
@@ -23,7 +20,7 @@ public class MainController {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
-        User n = new User();
+        SampleUser n = new SampleUser();
         n.setName(name);
         n.setEmail(email);
         userRepository.save(n);
@@ -31,7 +28,7 @@ public class MainController {
     }
 
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<User> getAllUsers() {
+    public @ResponseBody Iterable<SampleUser> getAllUsers() {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
     }
