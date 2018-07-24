e4bf2e1955ea30689934f0d026741cfe7276884e
Vladimir Svoboda <ladovina@seznam.cz>
2018-07-24 22:55:52 +0200

    Added all endpoints of the app, added simple test.

--- src/main/java/api/GoogleUserRepository.java
+++ src/main/java/api/GoogleUserRepository.java
@@ -1,10 +1,10 @@
-package hello;
+package api;
 
 import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<GoogleUser, Long> {
+public interface GoogleUserRepository extends CrudRepository<GoogleUser, Long> {
 
 }
---
+++ src/main/java/params/PostUsers.java
@@ -0,0 +1,22 @@
+package params;
+
+public class PostUsers {
+    private String googleUserId;
+    private String accessToken;
+
+    public String getGoogleUserId() {
+        return googleUserId;
+    }
+
+    public void setGoogleUserId(String googleUserId) {
+        this.googleUserId = googleUserId;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
