6870b5c9db5b0821e1689848ede5d8cb380e77bd
Vladimir Svoboda <ladovina@seznam.cz>
2018-07-26 17:36:14 +0200

    Only example app properties in git.

--- src/main/java/api/GoogleUserActivityRepository.java
+++ src/main/java/api/GoogleUserActivityRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-//TODO: move to repository
 public interface GoogleUserActivityRepository extends CrudRepository<GoogleUserActivity, Long> {
     List<GoogleUserActivity> findByGoogleUserGoogleUserId(String googleUserId);
 
--- src/main/java/api/GoogleUserRepository.java
+++ src/main/java/api/GoogleUserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-//TODO: move to repository
 public interface GoogleUserRepository extends CrudRepository<GoogleUser, String> {
 
 }
